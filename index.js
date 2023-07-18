af1a7f732907778ef64cdcb6bebd3fbcb5a22836
himanganimesh2 <himanganimesh2@gmail.com>
2023-07-18 23:58:14 +0530

    mongo db connected

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import express from "express"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
 
 const app = express()
+dotenv.config()
+
+const connect = () => {
+    mongoose.connect(process.env.MONGO).then(() => {
+        console.log("Connected to MongoDB")
+    })
+    .catch((error)=> {
+        throw error
+    })
+}
 
 app.listen(8000, ()=> {
+    connect()
     console.log("Connected!")
 })
